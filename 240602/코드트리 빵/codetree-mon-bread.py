2dac90d79c5da74533692de89bae418d4cf0d147
thisishwan2 <112103038+thisishwan2@users.noreply.github.com>
2024-06-02 20:46:49 +0900

    Title: 코드트리 빵, Time: 5090ms, Memory: 25MB, Status: Time Limit Exceed - CodeTree

--- 240602/코드트리 빵/codetree-mon-bread.py
+++ 240602/코드트리 빵/codetree-mon-bread.py
@@ -3,29 +3,30 @@ from collections import deque
 
 def find_basecamp(people_num):
     distance = 1e9
-    x = n
-    y = n
-    for i in range(n):
-        for j in range(n):
-            if arr[i][j] == 1:
-
-                # 거리가 최단 거리면
-                if abs(i - store[people_num][0]) + abs(j - store[people_num][1]) < distance:
-                    distance = abs(i - store[people_num][0]) + abs(j - store[people_num][1])
-                    x = i
-                    y = j
-
-                # 거리가 같으면
-                elif abs(i - store[people_num][0]) + abs(j - store[people_num][1]) == distance:
-                    if (x > i):  # 행이 작으면
-                        x = i
-                        y = j
-                    elif (x == i):  # 행이 같으면
-                        if (y > j):  # 열이 작은 경우만
-                            x = i
-                            y = j
-    return x, y
-
+    x=n
+    y=n
+    index = 0
+    for idx, val in enumerate(basecamp_candidate):
+        # 최단 거리면
+        if abs(store[people_num][0]-val[0]) + abs(store[people_num][1]-val[1]) < distance:
+            distance = abs(store[people_num][0]-val[0]) + abs(store[people_num][1]-val[1])
+            x=val[0]
+            y=val[1]
+            index = idx
+        # 거리가 같으면
+        elif abs(store[people_num][0]-val[0]) + abs(store[people_num][1]-val[1]) == distance:
+            # 행이 더 작은 베이스 캠프인 경우
+            if x>val[0]:
+                x=val[0]
+                y=val[1]
+                index = idx
+            # 행이 같으면, 열이 더 작은 베이스 캠프로
+            elif x==val[0]:
+                if y>val[1]:
+                    x=val[0]
+                    y=val[1]
+                    index = idx
+    return x,y,index
 
 dx = [-1, 0, 0, 1]
 dy = [0, -1, 1, 0]
@@ -35,6 +36,12 @@ arr = []
 
 for i in range(n):
     arr.append(list(map(int, input().split())))
+
+basecamp_candidate=[]
+for i in range(n):
+    for j in range(n):
+        if arr[i][j]==1:
+            basecamp_candidate.append([i,j])
 
 store = {}
 for i in range(m):
@@ -85,8 +92,10 @@ while True:
     time += 1
     if time <= m:
         # 베이스 캠프를 선정하고 거기에 위치시킨다.
-        base_x, base_y = find_basecamp(time)
+        base_x, base_y, index = find_basecamp(time)
         arr[base_x][base_y] = -1
+        # 베이스 캠프 후보에서도 지운다.
+        basecamp_candidate.pop(index)
         q.append([base_x, base_y, time])
 
     if len(q) == 0:
