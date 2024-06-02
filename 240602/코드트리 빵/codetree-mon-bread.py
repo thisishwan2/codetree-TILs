66c48cc557e6118a5f54f01fb8f5601e8b302d23
thisishwan2 <112103038+thisishwan2@users.noreply.github.com>
2024-06-02 21:52:49 +0900

    Title: 코드트리 빵, Time: 522ms, Memory: 28MB, Status: Passed - CodeTree

--- 240602/코드트리 빵/codetree-mon-bread.py
+++ 240602/코드트리 빵/codetree-mon-bread.py
@@ -1,32 +1,69 @@
 from collections import deque
 
+# 최단 거리를 찾기 위한 bfs
+def bfs(x,y,num):
 
-def find_basecamp(people_num):
+    q = deque()
+    q.append((x,y))
+    visited = [[-2 for _ in range(n)]for _ in range(n)]
+    visited[x][y]=-1
+
+    while q:
+        x,y = q.popleft()
+
+        if (x,y) == (store[num][0], store[num][1]):
+            return visited[x][y]
+
+
+        for i in range(4):
+            nx=x+dx[i]
+            ny=y+dy[i]
+
+            if 0<=nx<n and 0<=ny<n:
+                if arr[nx][ny]!=-1 and visited[nx][ny] == -2:
+                    if visited[x][y] == -1: # 첫 for문일때는 다음칸에 현재 이동하게된 방향의 인덱스를 넣어줌
+                        visited[nx][ny] = i
+                    else:
+                        visited[nx][ny] = visited[x][y]
+                    q.append([nx, ny])
+
+def find_basecamp(num):
+    store_x, store_y = store[num]
     distance = 1e9
-    x=n
-    y=n
-    index = 0
+    b_x=0
+    b_y=0
+    index=0
+
     for idx, val in enumerate(basecamp_candidate):
-        # 최단 거리면
-        if abs(store[people_num][0]-val[0]) + abs(store[people_num][1]-val[1]) < distance:
-            distance = abs(store[people_num][0]-val[0]) + abs(store[people_num][1]-val[1])
-            x=val[0]
-            y=val[1]
-            index = idx
-        # 거리가 같으면
-        elif abs(store[people_num][0]-val[0]) + abs(store[people_num][1]-val[1]) == distance:
-            # 행이 더 작은 베이스 캠프인 경우
-            if x>val[0]:
-                x=val[0]
-                y=val[1]
-                index = idx
-            # 행이 같으면, 열이 더 작은 베이스 캠프로
-            elif x==val[0]:
-                if y>val[1]:
-                    x=val[0]
-                    y=val[1]
+        base_x, base_y = val
+
+        q=deque()
+        q.append([base_x,base_y,0])
+        visited = [[0 for _ in range(n)] for _ in range(n)]
+        visited[base_x][base_y] = 1
+
+        while q:
+            x,y,cnt = q.popleft()
+
+            if (x,y) == (store_x,store_y):
+                if distance > cnt:
+                    distance = cnt
+                    b_x=base_x
+                    b_y=base_y
                     index = idx
-    return x,y,index
+
+                break
+
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+
+                if 0 <= nx < n and 0 <= ny < n:
+                    if arr[nx][ny] != -1 and visited[nx][ny]==0:
+                        visited[nx][ny]=1
+                        q.append([nx,ny,cnt+1])
+
+    return b_x,b_y,index
 
 dx = [-1, 0, 0, 1]
 dy = [0, -1, 1, 0]
@@ -42,14 +79,13 @@ for i in range(n):
     for j in range(n):
         if arr[i][j]==1:
             basecamp_candidate.append([i,j])
+basecamp_candidate = sorted(basecamp_candidate, key = lambda x: (x[0], x[1]))
+# print(basecamp_candidate)
 
 store = {}
 for i in range(m):
     x, y = map(int, input().split())
     store[i + 1] = [x - 1, y - 1]
-
-# print(*arr, sep="\n")
-# print(store)
 
 time = 0
 q = deque()
@@ -60,23 +96,10 @@ while True:
     # 1번: 격자에 있는 사람들 모두가 본인이 가고 싶은 편의점을 향해 1칸 움직인다.
     for _ in range(len(q)):
         x, y, num = q.popleft()
-
-        # 최단 거리 변수 설정
-        distance = 1e9
-        next_x = 0
-        next_y = 0
-
-        # 최단 거리로 움직이는 방법을 찾고, 여러개면 우선순위에 따라 움직인다.
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-
-            if 0 <= nx < n and 0 <= ny < n:
-                if arr[nx][ny] != -1:
-                    if abs(store[num][0] - nx) + abs(store[num][1] - ny) < distance:
-                        next_x = nx
-                        next_y = ny
-                        distance = abs(store[num][0] - nx) + abs(store[num][1] - ny)
+        # 최단 거리로 움직이는 방법을 찾고, 여러개면 우선순위에 따라 움직인다.(이떄 막히는 칸을 고려해서 최단거리를 찾아야한다.)
+        dir = bfs(x,y,num)
+        next_x = x+dx[dir]
+        next_y = y+dy[dir]
 
         # 2번: 편의점에 도착하면 멈추고, 해당 칸을 못지나가게 만든다.(못지나가게 하는 것은 모든 사람이 지나간 뒤에 해야한다.)
         if [next_x, next_y] == store[num]:  # 편의점에 도착한 경우
@@ -92,10 +115,9 @@ while True:
     time += 1
     if time <= m:
         # 베이스 캠프를 선정하고 거기에 위치시킨다.
-        base_x, base_y, index = find_basecamp(time)
+        base_x, base_y, idx = find_basecamp(time)
         arr[base_x][base_y] = -1
-        # 베이스 캠프 후보에서도 지운다.
-        basecamp_candidate.pop(index)
+        basecamp_candidate.pop(idx)
         q.append([base_x, base_y, time])
 
     if len(q) == 0:
